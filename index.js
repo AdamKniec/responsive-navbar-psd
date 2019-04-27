997586e695ea90e1eafcc8a7a597937c0516ffd9
AdamKniec <adam.k.kniec@gmail.com>
2019-04-27 18:18:49 +0200

    changing the styles

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //Mobile menu handler
 (function(){
 
-    const header = document.querySelector('.header');    
+    const body = document.querySelector('body');
+    const main = document.querySelector('.main');
     const menuOpen = document.querySelector('.button-menu-open');
     const menuClose = document.querySelector('.button-menu-close');
     const mobileSideNav = document.querySelector('.navigation-details-wrapper');
 
-    header.addEventListener('click', toggleMobilenavbar);
+
+    body.addEventListener('click', toggleMobilenavbar);
     function toggleMobilenavbar(e){
-        let items = document.querySelectorAll('.header > div:not(.navigation-details-wrapper)');
-        items.forEach = (item) => {
-            console.log(item);
-        }
+        
 
         if(e.target === menuOpen || e.target.closest('.navigation-details-wrapper') ) {
             mobileSideNav.classList.add('active');
             menuOpen.classList.add('hidden');
             menuClose.classList.add('visible');
+            main.classList.add('darkened');
         } else {
             mobileSideNav.classList.remove('active');
             menuOpen.classList.remove('hidden');
