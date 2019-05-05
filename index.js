9e41d789e6208e5b7062370b1e6a394a5c471f06
AdamKniec <adam.k.kniec@gmail.com>
2019-05-05 22:41:18 +0200

    quick nav functionality fix

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 //Mobile menu handler
 (function(){
-    const nav = document.querySelector('.navbar');
+    const body = document.querySelector('body');
     const main = document.querySelector('.main');
     const menuOpen = document.querySelector('.button-menu-open');
     const menuClose = document.querySelector('.button-menu-close');
     const mobileSideNav = document.querySelector('.navigation-details-wrapper');
 
-    nav.addEventListener('click', toggleMobilenavbar);
+    body.addEventListener('click', toggleMobilenavbar);
 
     function toggleMobilenavbar(e){
         if(e.target === menuOpen || e.target.closest('.navigation-details-wrapper')) {
+            console.log('powinno zostac open')
             mobileSideNav.classList.add('active');
             menuOpen.classList.add('hidden');
             menuClose.classList.add('visible');
             main.classList.add('darkened');
         } else {
+            console.log('powinno zostac closed')
             mobileSideNav.classList.remove('active');
             menuOpen.classList.remove('hidden');
             menuClose.classList.remove('visible');
